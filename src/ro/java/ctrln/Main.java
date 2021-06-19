66327b070205a7d0f2c45cbed1a10c25f162df22
40755 <popa.gabriel97@yahoo.com>
2021-06-19 15:21:32 +0300

    Am adaugat Java Doc

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,7 +1,15 @@
 package ro.java.ctrln;
 
+import java.sql.SQLOutput;
+
 public class Main {
-    // JAVA_HOME
+
+    /**
+     * Metoda Main este metoda apelata de masina virtuala Java si care porneste o aplicatie Java.
+     * Putem sa transmitem argumente aditionale prin parametrul <b>args</b>.
+     * @param args Acesta este parametru cu care transmitem argumente programului nostru.
+     * @author Popa Gabriel
+     */
     public static void main(String[] args) {
 
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
